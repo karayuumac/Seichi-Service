6b27a14ce89e8eb9a4b2eb9c9205fc8c2c745f16
karayuumac <y-kara0706@docomo.ne.jp>
2019-07-20 17:43:24 +0900

    Googleアナリティクスのコードを追加

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,6 +16,16 @@
       padding-top: 70px;
     }
   </style>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144234149-1"></script>
+  <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-144234149-1');
+  </script>
 </head>
 
 <body>
