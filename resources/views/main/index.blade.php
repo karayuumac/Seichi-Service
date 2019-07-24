ae0b1c5a3721a22da5edf12f24da905a468a8a5d
karayuumac <y-kara0706@docomo.ne.jp>
2019-07-24 21:29:29 +0900

    メインページの配置を変更

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="row">
-    <div class="col-12">
+    <div class="col-10 mx-auto">
       <div class="card border-primary">
         <div class="card-header">
           <i class="fa fa-info-circle fa-fw"></i>
